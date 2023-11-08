030fb18cfb6bef1fbfd1feadff6532355f62c5a8
AmandaCupolaa <amanda.alves@sptech.school>
2023-11-08 11:56:41 -0300

    Adicoes velocimetro dash motorista

--- template/pages/dash-motorista/tela_nova.js
+++ template/pages/dash-motorista/tela_nova.js
@@ -1,7 +1,7 @@
-var indicator = document.querySelector("#indicator");
-var valueText = document.querySelector("#value-text");
-var randomBtn = document.querySelector("#random");
-var gauge = document.querySelector(".gauge");
+// Grafico temperatura cpu
+
+var indicator = document.querySelector("#indicator-Temperatura-Cpu");
+var valueText = document.getElementById("value-temperatura-cpu");
 
 function exibirCPU() {
     fetch(`/tablet/exibirCPU`)
@@ -10,10 +10,10 @@ function exibirCPU() {
           response.json().then(function (resposta) {
             console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
             arrayTablet = resposta;
-  
-            cardCPU.innerHTML += `<p>${arrayTablet[0].cpuUso}</p>`;
-            const frame1 = parseFloat(arrayTablet[0].cpuUso.replace(',', '.')); // Converte para número usando parseFloat
-            goTo1(frame1); // Chama goTo1 com o valor recebido como frame1
+
+            frame1 = arrayTablet[0].cpuUso;
+            temperaturaCPU(frame1);
+
           });
         } else {
           console.error("Nenhum dado encontrado ou erro na API");
@@ -23,59 +23,111 @@ function exibirCPU() {
         console.error(`Erro na obtenção dos dados p/ idEmpresa: ${error.message}`);
       });
     return false;
-  }
+} 
 
-document.querySelector("#value-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-
-  changeRate(document.querySelector(frame1).value);
-});
-
-randomBtn.addEventListener("click", function() {
-  let randomNum = Math.round(Math.random() * 100);
-  changeRate(randomNum);
-});
-
-function rateToDegrees(rate) {
-  var degrees = rate * 180 / 100 - 90;
-  return degrees;
-}
-
-function changeRate(rate) {
-  var degrees = rateToDegrees(rate);
+function temperaturaCPU(taxaTemperaturaCPU) {
+  var degrees = rateToDegrees(taxaTemperaturaCPU);
   indicator.style.transform = "rotate(" + degrees + "deg)";
-  changeText(valueText, rate);
+  frame1 = taxaTemperaturaCPU;
+
+  var formattedRate = parseFloat(frame1).toFixed(2);
+  
+  valueText.textContent = formattedRate.replace(/(\.0*$|0+$)/, '') + "%";
+
+  frame3 = frame1 * 1.2;
+    CPU(frame3);
+
+} setInterval(exibirCPU, 1000);
+
+function rateToDegrees(taxaTemperaturaCPU) {
+    var degrees = taxaTemperaturaCPU * 180 / 100 - 90;
+    return degrees;
 }
 
-function changeText(item, value) {
-  var current = parseInt(item.innerHTML.slice(0, item.innerHTML.length - 1));
-  if (current > value) {
-    invertedIterate(current, value);
-  } else {
-    iterate(current, value);
+// Grafico temperatura gpu
+
+var indicatorGpu = document.querySelector("#indicator-Temperatura-Gpu");
+var valueTextGpu = document.getElementById("value-temperatura-gpu");
+
+function exibirGPU() {
+    fetch(`/tablet/exibirGPU`)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (resposta) {
+            console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+            arrayTablet = resposta;
+
+            frame2 = arrayTablet[0].memoria;
+            temperaturaGPU(frame2);
+
+          });
+        } else {
+          console.error("Nenhum dado encontrado ou erro na API");
+        }
+      })
+      .catch(function (error) {
+        console.error(
+          `Erro na obtenção dos dados p/ idEmpresa: ${error.message}`
+        );
+      });
+    return false;
   }
+
+function temperaturaGPU(taxaTemperaturaGPU) {
+  var degrees = rateToDegrees(taxaTemperaturaGPU);
+  indicatorGpu.style.transform = "rotate(" + degrees + "deg)";
+  frame2 = taxaTemperaturaGPU;
+
+  var formattedRate = parseFloat(frame2).toFixed(2);
+  
+  valueTextGpu.textContent = formattedRate.replace(/(\.0*$|0+$)/, '') + "%";
+} setInterval(exibirGPU, 1000);
+
+function rateToDegrees(taxaTemperaturaGPU) {
+    var degrees = taxaTemperaturaGPU * 180 / 100 - 90;
+    return degrees;
 }
 
-function iterate(i, n) {
-  if (i <= n) {
-    valueText.innerHTML = i + "%";
-    setTimeout(iterate, 10, i + 1, n);
-  }
-}
-function invertedIterate(i, n) {
-  if (i >= n) {
-    valueText.innerHTML = i + "%";
-    setTimeout(invertedIterate, 10, i - 1, n);
-  }
+// Grafico cpu
+
+var indicatorCpu = document.querySelector("#indicatorCpu");
+var valueTextCpu = document.getElementById("valueCpu");
+
+function CPU(taxaCPU) {
+  var degrees = rateToDegrees(taxaCPU);
+  indicatorCpu.style.transform = "rotate(" + degrees + "deg)";
+  frame3 = taxaCPU;
+
+  var formattedRate = parseFloat(frame3).toFixed(2);
+  
+  valueTextCpu.textContent = formattedRate.replace(/(\.0*$|0+$)/, '') + "%";
+} setInterval(exibirGPU, 1000);
+
+function rateToDegrees(taxaCPU) {
+    var degrees = taxaCPU * 180 / 100 - 90;
+    return degrees;
 }
 
-function docReady(fn) {
-  if (
-    document.readyState === "complete" ||
-    document.readyState === "interactive"
-  ) {
-    setTimeout(fn, 1);
-  } else {
-    document.addEventListener("DOMContentLoaded", fn);
-  }
+// bateria
+
+navigator.getBattery()
+.then(function (battery) {
+  window.battery = battery;
+  window.porcentagem = document.querySelector('#porcentagem');
+  handleBattery();
+});
+  
+function handleBattery() {
+    if (battery) {
+        battery.addEventListener('chargingchange', updateBatteryStatus);
+        battery.addEventListener('levelchange', updateBatteryStatus);
+        updateBatteryStatus();
+    }
+}
+ 
+function updateBatteryStatus() {
+    if (battery.level * 100 === 100 ) porcentagem.style.width =  '97%';
+    else porcentagem.style.width = battery.level * 100 + '%';
+    
+    porcentagem.className = battery.charging ? 'charging' : 'notCharging';
 }
