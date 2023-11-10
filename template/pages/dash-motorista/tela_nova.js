d68b6bf351a24dacd06c7e020992af0505a975fe
AmandaCupolaa <amanda.alves@sptech.school>
2023-11-09 23:10:14 -0300

    adicao graficos do dash motorista

--- template/pages/dash-motorista/tela_nova.js
+++ template/pages/dash-motorista/tela_nova.js
@@ -79,9 +79,12 @@ function temperaturaGPU(taxaTemperaturaGPU) {
   frame2 = taxaTemperaturaGPU;
 
   var formattedRate = parseFloat(frame2).toFixed(2);
+
+  frame4 = frame2 * 1.2;
+    GPU(frame4);
   
   valueTextGpu.textContent = formattedRate.replace(/(\.0*$|0+$)/, '') + "%";
-} setInterval(exibirGPU, 1000);
+} setInterval(temperaturaGPU, 1000);
 
 function rateToDegrees(taxaTemperaturaGPU) {
     var degrees = taxaTemperaturaGPU * 180 / 100 - 90;
@@ -98,36 +101,162 @@ function CPU(taxaCPU) {
   indicatorCpu.style.transform = "rotate(" + degrees + "deg)";
   frame3 = taxaCPU;
 
-  var formattedRate = parseFloat(frame3).toFixed(2);
+  var formattedRate = parseFloat(frame3).toFixed(1);
   
   valueTextCpu.textContent = formattedRate.replace(/(\.0*$|0+$)/, '') + "%";
-} setInterval(exibirGPU, 1000);
+
+} setInterval(CPU, 1000);
 
 function rateToDegrees(taxaCPU) {
     var degrees = taxaCPU * 180 / 100 - 90;
     return degrees;
 }
 
+// Grafico GPU 
+
+var indicator_GPU = document.querySelector("#indicator_GPU");
+var valueText_GPU = document.getElementById("value_GPU");
+
+function GPU(taxaGPU) {
+  var degrees = rateToDegrees(taxaGPU);
+  indicator_GPU.style.transform = "rotate(" + degrees + "deg)";
+  frame4 = taxaGPU;
+
+  var formattedRate = parseFloat(frame4).toFixed(1);
+  
+  valueText_GPU.textContent = formattedRate.replace(/(\.0*$|0+$)/, '') + "%";
+} setInterval(GPU, 1000);
+
+function rateToDegrees(taxaGPU) {
+    var degrees = taxaGPU * 180 / 100 - 90;
+    return degrees;
+}
+
 // bateria
 
-navigator.getBattery()
-.then(function (battery) {
-  window.battery = battery;
-  window.porcentagem = document.querySelector('#porcentagem');
-  handleBattery();
-});
+var arrayTablet = []; 
+
+function exibirBateria() {
+  fetch(`/tablet/exibirBateria`)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (resposta) {
+          console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+          arrayTablet = resposta;
+          
+          batteryPercentage = arrayTablet[0].bateriaNivel.toString().slice(0, 2)
+          initBattery(batteryPercentage)
+
+        });
+      } else {
+        console.error("Nenhum dado encontrado ou erro na API");
+      }
+    })
+    .catch(function (error) {
+      console.error(`Erro na obtenção dos dados p/ idEmpresa: ${error.message}`);
+    });
+  return false;
+} 
+
+initBattery()
+
+function initBattery(){
+    const batteryLiquid = document.querySelector('.battery__liquid'),
+          batteryStatus = document.querySelector('.battery__status'),
+          batteryHTML = document.querySelector('#batteryPercentage')
+
+          let level = Math.floor(batteryPercentage)
+          batteryHTML.innerHTML = level + '%'
+
+            batteryLiquid.style.height = `${parseInt(batteryPercentage )}%`
+
+            if(batteryPercentage == 100){ 
+                batteryStatus.innerHTML = `Full battery <i class="ri-battery-2-fill green-color"></i>`
+                batteryLiquid.style.height = '103%' 
+            }
+            else if(batteryPercentage <= 20 &! batteryPercentage.charging){ 
+                batteryStatus.innerHTML = `Low battery <i class="ri-plug-line animated-red"></i>`
+            }
+            else if(batteryPercentage.charging){ 
+                batteryStatus.innerHTML = `Charging... <i class="ri-flashlight-line animated-green"></i>`
+            }
+            else{
+                batteryStatus.innerHTML = ''
+            }
+            
+            if(batteryPercentage <=20){
+                batteryLiquid.classList.add('gradient-color-red')
+                batteryLiquid.classList.remove('gradient-color-orange','gradient-color-yellow','gradient-color-green')
+            }
+            else if(batteryPercentage <= 40){
+                batteryLiquid.classList.add('gradient-color-orange')
+                batteryLiquid.classList.remove('gradient-color-red','gradient-color-yellow','gradient-color-green')
+            }
+            else if(batteryPercentage <= 80){
+                batteryLiquid.classList.add('gradient-color-yellow')
+                batteryLiquid.classList.remove('gradient-color-red','gradient-color-orange','gradient-color-green')
+            }
+            else{
+                batteryLiquid.classList.add('gradient-color-green')
+                batteryLiquid.classList.remove('gradient-color-red','gradient-color-orange','gradient-color-yellow')
+            }
+        } setInterval(initBattery,10000)
+// alertas
+
+// grafico
   
-function handleBattery() {
-    if (battery) {
-        battery.addEventListener('chargingchange', updateBatteryStatus);
-        battery.addEventListener('levelchange', updateBatteryStatus);
-        updateBatteryStatus();
+const exibirAlerta = document.getElementById('exibirAlerta');
+const alerta = document.getElementById('alerta');
+const fecharAlerta = document.getElementById('fecharAlerta');
+
+exibirAlerta.addEventListener('click', () => {
+    alerta.style.display = 'block';
+});
+
+fecharAlerta.addEventListener('click', () => {
+    alerta.style.display = 'none';
+});
+
+// Gráfico
+var options = {
+    chart: {
+        height: 390,
+        width: 800,
+        type: "area"
+    },
+    dataLabels: {
+        enabled: false
+    },
+    series: [
+        {
+            name: "Series 1",
+            data: [45, 52, 38, 45, 19, 23, 2]
+        }
+    ],
+    fill: {
+        type: "gradient",
+        gradient: {
+            shadeIntensity: 1,
+            opacityFrom: 0.7,
+            opacityTo: 0.9,
+            stops: [0, 90, 100]
+        }
+    },
+    xaxis: {
+        categories: [
+            "01 Jan",
+            "02 Jan",
+            "03 Jan",
+            "04 Jan",
+            "05 Jan",
+            "06 Jan",
+            "07 Jan"
+        ]
     }
-}
- 
-function updateBatteryStatus() {
-    if (battery.level * 100 === 100 ) porcentagem.style.width =  '97%';
-    else porcentagem.style.width = battery.level * 100 + '%';
-    
-    porcentagem.className = battery.charging ? 'charging' : 'notCharging';
-}
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+
+
